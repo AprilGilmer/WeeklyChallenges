e839709300baced8237baa06b592dda1934ed310
April Gilmer <aprilgilmer1@gmail.com>
2025-02-23 19:00:21 -0600

    completed Weekly challenge 3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,101 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (var val in vals)
+            {
+                if (val == false)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            // We want to get the sum of the odd numbers in the list numbers
+            // Then we check if the sum is an odd number
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            List<int> odds = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 1 || number % 2 == -1)
+                {
+                    odds.Add(number);
+                }
+            }
+
+            int sum = 0;
+            foreach (int odd in odds)
+            {
+                sum += odd;
+            }
+
+            if (sum % 2 == 1 || sum % 2 == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            return password.Any(char.IsNumber)
+                && password.Any(char.IsUpper)
+                && password.Any(char.IsLower);
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[^1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return divisor == 0 ? 0 : dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[^1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            //  int[] myArray = new int[50];
+            // int index = 0;
+            var odds = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    odds.Add(i);
+                }
+            }
+
+            return odds.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
